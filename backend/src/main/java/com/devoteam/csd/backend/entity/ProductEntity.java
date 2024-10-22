5a7806ad70984bcef96da0d684ec71e2a384821d
VSCHNE <volker.schneider@devoteam.com>
2024-10-22 11:46:47 +0200

    Ticket #5: 'product' table id and sequence added, name and description lengths enhanced

--- backend/src/main/java/com/devoteam/csd/backend/entity/ProductEntity.java
+++ backend/src/main/java/com/devoteam/csd/backend/entity/ProductEntity.java
@@ -2,19 +2,35 @@ package com.devoteam.csd.backend.entity;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
+import java.sql.Blob;
 
 @Entity
 @Table(name = "product")
 public class ProductEntity {
 
   @Id
+  @SequenceGenerator(name = "product_sequence", sequenceName = "product_sequence", allocationSize = 1, initialValue = 1)
+  @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence")
   @Column(name = "id")
+  private Long id;
+
   private int productId;
   private String category;
+
+  @Column(name = "name", length = 2000)
   private String name;
+
+  @Column(name = "description", length = 10000)
   private String description;
+
+  @Lob
+    private Blob image;
 
   public int getProductId() {
     return productId;
@@ -46,5 +62,13 @@ public class ProductEntity {
 
   public void setDescription(String description) {
     this.description = description;
+  }
+
+  public Blob getImage() {
+    return image;
+  }
+
+  public void setImage(Blob image) {
+    this.image = image;
   }
 }
