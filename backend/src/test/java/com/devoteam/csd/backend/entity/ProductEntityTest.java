ff3cabd556838cd6639da173f6c7eaee010ec3db
VSCHNE <volker.schneider@devoteam.com>
2024-10-24 12:36:06 +0200

    ProductEntity service tests completed

--- backend/src/test/java/com/devoteam/csd/backend/entity/ProductEntityTest.java
+++ backend/src/test/java/com/devoteam/csd/backend/entity/ProductEntityTest.java
@@ -1,11 +1,14 @@
 package com.devoteam.csd.backend.entity;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import java.sql.Blob;
 import org.junit.jupiter.api.Test;
 
 public class ProductEntityTest {
   @Test
-  public void test_getterSetter() {
+  public void test_getterSetter() throws Exception {
     ProductEntity product = new ProductEntity();
     product.setId(3L);
     assertEquals(3L, product.getId());
@@ -15,6 +18,11 @@ public class ProductEntityTest {
     assertEquals("Pay to Fuel", product.getName());
     product.setDescription("Tarifdschungel, nein danke!");
     assertEquals("Tarifdschungel, nein danke!", product.getDescription());
+
+    byte[] bytes = "This could be an image".getBytes();
+    Blob image = new javax.sql.rowset.serial.SerialBlob(bytes);
+    product.setImage(image);
+    assertEquals(image, product.getImage());
   }
 
   @Test
@@ -24,5 +32,6 @@ public class ProductEntityTest {
     assertEquals("Infotainment Apps", product.getCategory());
     assertEquals("Pay to Fuel", product.getName());
     assertEquals("Tarifdschungel", product.getDescription());
+    assertNull(product.getImage());
   }
 }
--- backend/src/test/java/com/devoteam/csd/backend/service/ProductServiceImplTest.java
+++ backend/src/test/java/com/devoteam/csd/backend/service/ProductServiceImplTest.java
@@ -2,6 +2,7 @@ package com.devoteam.csd.backend.service;
 
 import com.devoteam.csd.backend.entity.ProductEntity;
 import com.devoteam.csd.backend.repository.ProductRepository;
+import java.sql.Blob;
 import java.util.ArrayList;
 import java.util.List;
 import org.junit.jupiter.api.Test;
@@ -13,6 +14,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.Mockito.when;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.util.Optional;
 
 @SpringBootTest
 public class ProductServiceImplTest {
@@ -69,6 +72,56 @@ public class ProductServiceImplTest {
 
     // do the test
     List<ProductEntity> result = productService.getProducts();
+    assertNull(result);
+  }
+
+  /**
+   * Tests getting a product image
+   */
+  @Test
+  void test_getProductImage_ok() throws Exception {
+    ProductEntity product = new ProductEntity(1L, "Infotainment", "Traffication",
+        "Die App verbessert Ihre Sicherheit");
+
+    byte[] bytes = "My image".getBytes();
+    Blob image = new javax.sql.rowset.serial.SerialBlob(bytes);
+    product.setImage(image);
+
+    when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+    byte[] result = productService.getProductImage(1L);
+    assertEquals("My image", new String(result));
+  }
+
+  /**
+   * Tests getting a product image where the product does not have an image
+   */
+  @Test
+  void test_getProductImage_noImage() {
+    ProductEntity product = new ProductEntity(1L, "Infotainment", "Traffication",
+        "Die App verbessert Ihre Sicherheit");
+
+    when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+    byte[] result = productService.getProductImage(1L);
+    assertNull(result);
+  }
+
+  /**
+   * Tests getting a product image where the product was not found
+   */
+  @Test
+  void test_getProductImage_noProductFound() throws Exception {
+    when(productRepository.findById(1L)).thenReturn(Optional.empty());
+    byte[] result = productService.getProductImage(1L);
+    assertNull(result);
+  }
+
+  /**
+   * Tests getting a product image where an exception is thrown while getting the image
+   */
+  @Test
+  void test_getProductImage_exceptionGettingImage() {
+    when(productRepository.findById(1L)).thenThrow(new NullPointerException());
+    byte[] result = productService.getProductImage(2L);
     assertNull(result);
   }
 }
